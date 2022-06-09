2cdeeea6719e3b4e668d2ab1bb7b7ce0ef551627
SibyPresidento95 <103515873+SibyPresidento95@users.noreply.github.com>
2022-06-09 23:43:41 +0200

    My testing page

---
+++ Js/sligSettings.js
@@ -0,0 +1,25 @@
+$('.team-carousel').slick({
+  arrows: false,
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+  mobileFirst: true,
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+//   responsive: [
+
+//     {
+//         breakpoint: 768,
+//         settings: { slidesToshow: 2}
+//     },
+
+//      {
+//         breakpoint: 992,
+//         settings: { slidesToshow: 3}
+//     }
+
+//   ]
+});
+
+// settings: "unslick"
+
